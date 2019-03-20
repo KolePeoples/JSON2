e28a63a69c12b557e96908b9696252539a470fc7
KolePeoples <kolepeoples@gmail.com>
2019-03-20 08:45:41 -0500

    Initial commit

---
+++ JSON2/DataManager.swift
@@ -0,0 +1,25 @@
+//
+//  DataManager.swift
+//  JSON2
+//
+//  Created by Kole Peoples on 3/20/19.
+//  Copyright © 2019 Kole Peoples. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+
+        let MYJSONURL = "https://api.myjson.com/bins/1e5uji"
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
